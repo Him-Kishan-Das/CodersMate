e95427685f0042b8b14017881939dd1c4d212ab9
Him-Kishan_Das <himkishandas456@gmail.com>
2022-11-02 23:15:49 +0530

    third commit

--- home.php
+++ home.php
@@ -12,10 +12,10 @@
 <body>
 
     <?php
-        include './Components/Navbar.php'
+    include './Components/Navbar.php'
     ?>
 
-    
+
     <!-- Section - 1 -->
     <div class="section">
         <div class="part1">
@@ -27,11 +27,36 @@
             </div>
         </div>
         <div>
-        <img class="section_img" src="./img/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg" alt="codingImage">
+            <img class="section_img" src="./img/oskar-yildiz-cOkpTiJMGzA-unsplash.jpg" alt="codingImage">
         </div>
     </div>
 
+
+    <!-- Section - 2 -->
+    <div class="section2">
+        <div class="block" id="categories">
+            <img src="./icons/code-solid.svg" alt="code_icon" class="block_icon">
+            <p class="num">10</p>
+            <p class="name">Topics</p>
+        </div>
+        <div class="block" id="queries">
+            <img src="./icons/clipboard-question-solid.svg" alt="clipboard_icon" class="block_icon">
+            <p class="num">50</p>
+            <p class="name">Queries</p>
+        </div>
+        <div class="block" id="replies">
+            <img src="./icons/reply-solid.svg" alt="reply_icon" class="block_icon">
+            <p class="num">100</p>
+            <p class="name">Replies</p>
+        </div>
+        <div class="block" id="registered_Users">
+            <img src="./icons/user-solid.svg" alt="user_icon" class="block_icon">
+            <p class="num">13</p>
+            <p class="name">Registered Users</p>
+        </div>
+    </div>
 </body>
 
 <script src="./scripts/home.js"></script>
+
 </html>
