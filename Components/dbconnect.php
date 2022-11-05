c4106e2434eaccc5ddb8e8c6a34b9e3eb80ecc36
Him-Kishan_Das <himkishandas456@gmail.com>
2022-11-05 22:06:25 +0530

    first commit

---
+++ Components/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "codersname";
+
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+?>
--- topics.php
+++ topics.php
@@ -11,20 +11,31 @@
 
 <body>
     <?php
+    include './Components/dbconnect.php';
     include './Components/Navbar.php';
     ?>
+
     <div class="main">
         <div class="section1">
+
             <h1>Programming Languages</h1>
 
             <!-- Programming Languages  -->
             <div class="program">
-
-                <div class="card">
-                    <img src="./img/python.jpg" alt="python image" class="card-img">
-                    <div class="card_container">
-                        <h1 class="card_heading">Python</h1>
-                        <p class="card_para">Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Python is dynamically-typed and garbage-collected. It supports multiple programming paradigms, including structured, object-oriented and functional </p>
+                <?php
+                $sql = "SELECT * FROM `categories`";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $id = $row['category_id'];
+                    $title = $row['category_name'];
+                    $desc = $row['category_description'];
+                    $type = $row['category_type'];
+                    if ($type == 'programming') {
+                        echo '<div class="card">
+                        <img src="./img/'. $title .'.jpg" alt="'. $title .' image" class="card-img">
+                        <div class="card_container">
+                        <h1 class="card_heading">' . $title . '</h1>
+                        <p class="card_para">' . substr($desc, 0, 200) .'...</p>
                         <div class="card-details">
                             <button class="card_btn">View Queries</button>
                             <div class="stats">
@@ -39,20 +50,30 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>';
+                    }
+                }
+                ?>
 
             </div>
-
 
             <!-- Frameworks  -->
             <h1>FrameWorks</h1>
             <div class="program">
-
-                <div class="card">
-                    <img src="./img/django.jpg" alt="python image" class="card-img">
+            <?php
+            $sql = "SELECT * FROM `categories`";
+            $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $id = $row['category_id'];
+                    $title = $row['category_name'];
+                    $desc = $row['category_description'];
+                    $type = $row['category_type'];
+                    if ($type == 'framework') {
+                    echo '<div class="card">
+                    <img src="./img/'. $title .'.jpg" alt="'. $title .' image" class="card-img">
                     <div class="card_container">
-                        <h1 class="card_heading">Django</h1>
-                        <p class="card_para">Django is a free and open-source, Python-based web framework that follows the model–template–views architectural pattern. It is maintained by the Django Software Foundation, an independent organization established in the US as a 501 non-profit. </p>
+                        <h1 class="card_heading">'. $title .'</h1>
+                        <p class="card_para">' . substr($desc, 0, 200) .'...</p>
                         <div class="card-details">
                             <button class="card_btn">View Queries</button>
                             <div class="stats">
@@ -67,7 +88,10 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>';
+                    }
+                }
+                ?>
 
             </div>
         </div>
