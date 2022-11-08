219fc45f0b2cdac96fa3b3a133b16715a4783ba0
Him-Kishan_Das <himkishandas456@gmail.com>
2022-11-08 21:05:40 +0530

    design canges to replies and query page

--- query.php
+++ query.php
@@ -17,14 +17,15 @@ $cat_name = $_GET['catName'];
 
     <?php
     include './Components/Navbar.php';
+    include './Components/dbconnect.php'
     ?>
 
     <div class="container">
 
         <div class="block">
 
-            <h2 class="title">Welcome to Python Forums</h2>
-            <p class="para">Python related queries and topics should be posted/found in this page of the forum.</p>
+            <h2 class="title">Welcome to <?php echo $cat_name; ?> Forums</h2>
+            <p class="para"><?php echo $cat_name; ?> related queries and topics should be posted/found in this page of the forum.</p>
             <button class="queryBtn" onclick="askQuery()">Ask a Query</button>
         </div>
 
@@ -45,21 +46,35 @@ $cat_name = $_GET['catName'];
         <div class="queryList">
             <h2 class="queryListHeading">Browse Queries</h2>
 
-            <hr>
+            <!-- For displaying the query list from the database -->
+            <?php
+            $cat_name = $_GET['catName'];
+            $sql = "SELECT * FROM `queries` WHERE query_cat_name = '$cat_name'";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                $id = $row['query_id'];
+                $title = $row['query_title'];
+                $desc = $row['query_desc'];
+                $query_user_id = $row['query_user_id'];
+                $query_time = $row['timestamp'];
+
+                echo '<hr>
             <div class="query">
                 <img class="userProfile" src="./icons/userdefault.png" alt="user profile">
                 <div class="queryDetails">
-                    <h3 class="queryTitle">
-                        Unable to install pyaudio. 
+                    <h3 ><a class="queryTitle" href="./replies.php?queryId=' . $id .'"> '. $title . ' </a>
                     </h3>
-                    <p class="userName">him-kishan . Oct 17 2022</p>
+                    <p class="queryDesc">' . substr($desc, 0, 100) .'...</p>
+                    <p class="userName">him-kishan . '. $query_time .'</p>
                 </div>
                 <div class="queryStats">
                     <h4 class="queryStatsTitle">Answers: </h4>
                     <p class="queryStatsValue">10</p>
                 </div>
-            </div>
-        
+            </div>';
+            }
+            ?>
+
 
 
         </div>
@@ -71,4 +86,5 @@ $cat_name = $_GET['catName'];
     ?>
 </body>
 <script src="./scripts/query.js"></script>
+
 </html>
---
+++ replies.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coder's Mate - Replies</title>
+    <link rel="stylesheet" href="./styles/replies.css">
+</head>
+
+<body>
+    <?php
+    include './Components/Navbar.php';
+    include './Components/dbconnect.php'
+    ?>
+
+    <div class="container">
+        <div class="block">
+
+
+
+            <h2 class="title">Welcome to Forums</h2>
+            <p class="para"> related queries and topics should be posted/found in this page of the forum.</p>
+            <div class="user">
+                <h2 class="posted">Posted by : </h2>
+                <p class="userName">him-kishan . </p>
+            </div>
+            <button class="replyBtn" onclick="askQuery()">Post Reply</button>
+        </div>
+
+        <div id="queryForm">
+            <form action="" class="formFields" method="post">
+                <label for="queryDesc" class="queryText">Post Reply</label>
+
+                <textarea placeholder="Enter a Solution" class="queryInput" id="queryInputDesc" name="queryDesc" cols="30" rows="5"></textarea>
+
+                <button class="replySubmitBtn">Submit</button>
+            </form>
+        </div>
+
+        <div class="replyList">
+            <h2 class="replyListHeading">Replies</h2>
+
+
+            <hr>
+            <div class="query">
+
+                <div class="replyDetails">
+                    <p class="replyDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, magnam voluptas vel quidem facere aperiam sapiente nobis fugit sint, officiis deserunt eaque esse voluptatem at excepturi harum consequatur maiores voluptatum.
+                        dhsfjh sdajf
+                        sdjhf jkhsadjkfh
+                    </p>
+                    <div class="user">
+                        <img class="userProfile" src="./icons/userdefault.png" alt="user profile">
+                        <p class="userName">him-kishan . </p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <?php
+    include './Components/Footer.php';
+    ?>
+</body>
+<script src="./scripts/query.js"></script>
+
+</html>
