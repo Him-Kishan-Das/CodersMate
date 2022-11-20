b556704e087902d60a75d8b791f83183d1cb7140
Him-Kishan_Das <himkishandas456@gmail.com>
2022-11-20 19:47:18 +0530

    development process;

--- admin.php
+++ admin.php
@@ -1,3 +1,7 @@
+<?php
+        $username = $_GET['userid'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,7 +29,7 @@
     <!-- Header Section  -->
     <div class="header">
         <p class="headerTitle">ADMIN PANEL</p>
-        <p class="username"><b>admin:</b> him_kishan_das</p>
+        <p class="username"><b>admin:</b> <?php echo $username ?></p>
     </div>
 
     <!-- Section 2  -->
@@ -38,22 +42,21 @@
         </div>
 
         <?php
-            if(isset($_POST['add'])){
-                $catName = $_POST['categoryName'];
-                $catType = $_POST['categoryType'];
-                $catDesc = $_POST['categoryDesc'];
+        if (isset($_POST['add'])) {
+            $catName = $_POST['categoryName'];
+            $catType = $_POST['categoryType'];
+            $catDesc = $_POST['categoryDesc'];
 
-                $select = "SELECT * FROM `categories` ORDER BY category_id DESC LIMIT 1";
-                $catresult = mysqli_query($conn, $select);
-                $row3 = mysqli_fetch_assoc($catresult);
-                $catid = $row3['category_id'];
-                $catid++;
+            $select = "SELECT * FROM `categories` ORDER BY category_id DESC LIMIT 1";
+            $catresult = mysqli_query($conn, $select);
+            $row3 = mysqli_fetch_assoc($catresult);
+            $catid = $row3['category_id'];
+            $catid++;
 
-     
-                $insert = "INSERT INTO `categories` (`category_id`, `category_name`, `category_description`, `category_type`, `created`) VALUES ('$catid', '$catName','$catDesc',  '$catType',current_timestamp())";
-                $final = mysqli_query($conn, $insert);
-            }
-            
+            $insert = "INSERT INTO `categories` (`category_id`, `category_name`, `category_description`, `category_type`, `created`) VALUES ('$catid', '$catName','$catDesc',  '$catType',current_timestamp())";
+            $final = mysqli_query($conn, $insert);
+        }
+
 
         ?>
         <div id="categorySec" class="section2">
@@ -61,7 +64,7 @@
                 <div class="category">
                     <div class="cat">
                         <label for="categoryName" class="categoryText">Category Name</label>
-                        <input required name="categoryName" type="text" class="categoryInput" id="categoryInputName" placeholder="Enter the title">
+                        <input required name="categoryName" type="text" id="categoryInputName" placeholder="Enter the title">
                     </div>
                     <div class="cat">
                         <label for="categoryType" class="categoryText">Category Type</label>
@@ -77,32 +80,76 @@
                     <textarea name="categoryDesc" id="categoryInputDesc" cols="30" rows="10"></textarea>
                 </div>
 
-                <button name="add">Add</button>
+                <button name="add" class="addSubmit">Add</button>
             </form>
-        </div>
 
-
-
-        <div id="querySec" class="section2">
+            <h2 class="catHeading">Categories</h2>
 
             <?php
 
-            // Deleting 
-            if (isset($_POST['delete'])) {
-                $deleteId = $_POST['id'];
-                $query = "DELETE FROM `queries` WHERE query_id = $deleteId";
-                $result = mysqli_query($conn, $query);
+            // updating categories 
+            if(isset($_POST['updateCat'])){
+                $title = $_POST['catTitle'];
+                $desc = $_POST['catDesc'];
+                $id = $_POST['catid'];
+                $update = "UPDATE `categories` SET `category_name` = '$title', `category_description` = '$desc' WHERE category_id = '$id'";
+                $updateQuery = mysqli_query($conn, $update);
             }
 
-            $sql = "SELECT * FROM `queries` ORDER BY query_time DESC LIMIT 10";
-            $result = mysqli_query($conn, $sql);
-            while ($row = mysqli_fetch_assoc($result)) {
-                $queryTitle = $row['query_title'];
-                $queryDesc = $row['query_desc'];
-                $queryId = $row['query_id'];
-                $catname = $row['query_cat_name'];
 
-                echo '<div class="query">
+            //deleting categories
+            if(isset($_POST['deleteCat'])){
+                $deleteid = $_POST['catid'];
+                $deleteCat = "DELETE FROM `categories` WHERE category_id = '$deleteid'";
+                mysqli_query($conn, $deleteCat);
+            }
+
+            $sql4 = "SELECT * FROM `categories`";
+            $result4 = mysqli_query($conn, $sql4);
+            while($row4 = mysqli_fetch_assoc($result4)){
+
+            echo '<form class="categories" method="post">
+                <label for="catTitle" class="categoriesText">Title</label>
+                <input class="categoryInput" value="'. $row4['category_name'] .'" name="catTitle">
+
+                <label for="catDesc" class="categoriesText">Description</label>
+                <textarea class="categoryInput" name="catDesc">'. $row4['category_description'] .'</textarea>
+
+
+                <input type="hidden" name="catid" value="'. $row4['category_id'] .'">
+                <div class="catbtns">
+                    <button id="updateCat" class="btn" name="updateCat">Update</button>
+                    <button id="deleteCat" class="btn" name="deleteCat">Delete</button>
+                </div>
+            </form>';
+            }
+            ?>
+
+        </div>
+    </div>
+
+
+
+    <div id="querySec" class="section2">
+
+        <?php
+
+        // Deleting 
+        if (isset($_POST['delete'])) {
+            $deleteId = $_POST['id'];
+            $query = "DELETE FROM `queries` WHERE query_id = $deleteId";
+            $result = mysqli_query($conn, $query);
+        }
+
+        $sql = "SELECT * FROM `queries` ORDER BY query_time DESC LIMIT 10";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $queryTitle = $row['query_title'];
+            $queryDesc = $row['query_desc'];
+            $queryId = $row['query_id'];
+            $catname = $row['query_cat_name'];
+
+            echo '<div class="query">
                 
                 <!-- <img class="userProfile" src="./icons/userdefault.png" alt="user profile"> -->
                 <div class="queryDetails">
@@ -120,44 +167,44 @@
                 <input type="hidden" name="id" value="' . $queryId . '">
                 <input type="submit" class="deleteBtn" value="Delete" name="delete">
             </form>';
-            }
-            ?>
-        </div>
+        }
+        ?>
+    </div>
 
 
 
-        <div id="replySec" class="section2">
+    <div id="replySec" class="section2">
 
 
-            <?php
+        <?php
 
-            // deleting replies 
-            if (isset($_POST['replyDelete'])) {
-                $replyDeleteId = $_POST['replyid'];
-                $deleteReply = "DELETE FROM `replies` WHERE reply_id = $replyDeleteId";
-                $deleteResult = mysqli_query($conn, $deleteReply);
-            }
-
-
-            $sql1 = "SELECT * FROM `replies` ORDER BY reply_time DESC LIMIT 14";
-            $result1 = mysqli_query($conn, $sql1);
-
-            while ($row = mysqli_fetch_assoc($result1)) {
-                $replyDesc = $row['reply_desc'];
-                $reply_query_Id = $row['query_id'];
-                $replyId = $row['reply_id'];
-                $replyUserId = $row['reply_user_id'];
-
-                $sql2 = "SELECT * FROM `queries` WHERE query_id='$reply_query_Id'";
-                $result2 = mysqli_query($conn, $sql2);
-                $row1 = mysqli_fetch_assoc($result2);
+        // deleting replies 
+        if (isset($_POST['replyDelete'])) {
+            $replyDeleteId = $_POST['replyid'];
+            $deleteReply = "DELETE FROM `replies` WHERE reply_id = $replyDeleteId";
+            $deleteResult = mysqli_query($conn, $deleteReply);
+        }
 
 
-                $sql3 = "SELECT * FROM `users` WHERE user_id = $replyUserId";
-                $result3 = mysqli_query($conn, $sql3);
-                $row2 = mysqli_fetch_assoc($result3);
+        $sql1 = "SELECT * FROM `replies` ORDER BY reply_time DESC LIMIT 14";
+        $result1 = mysqli_query($conn, $sql1);
 
-                echo '<div class="reply">
+        while ($row = mysqli_fetch_assoc($result1)) {
+            $replyDesc = $row['reply_desc'];
+            $reply_query_Id = $row['query_id'];
+            $replyId = $row['reply_id'];
+            $replyUserId = $row['reply_user_id'];
+
+            $sql2 = "SELECT * FROM `queries` WHERE query_id='$reply_query_Id'";
+            $result2 = mysqli_query($conn, $sql2);
+            $row1 = mysqli_fetch_assoc($result2);
+
+
+            $sql3 = "SELECT * FROM `users` WHERE user_id = $replyUserId";
+            $result3 = mysqli_query($conn, $sql3);
+            $row2 = mysqli_fetch_assoc($result3);
+
+            echo '<div class="reply">
                         <div class="replyDetails">
                         <h3 ><a class="queryTitle" href="">' . $row1['query_title'] . '</a></h3>
                         
@@ -176,11 +223,11 @@
                             <input type="submit" class="deleteBtn" value="Delete" name="replyDelete">
                         </form>
                     </div>';
-            }
+        }
 
-            ?>
+        ?>
 
-        </div>
+    </div>
 </body>
 
 <script src="./scripts/admin.js"></script>
